5277ecd14c6a5587d1786e1de8fe22f88a28b479
VSCLab-Wun <chiehwun5200@gmail.com>
2022-04-26 00:37:09 +0800

    encode shift ver

--- RTL/LZ77_Encoder.v
+++ RTL/LZ77_Encoder.v
@@ -1,4 +1,9 @@
-module LZ77_Encoder(clk,reset,chardata,valid,encode,finish,offset,match_len,char_nxt);
+/* Author: rubato Wun
+img0: 475,350 ns
+img1: 461,010 ns
+img2: 261,180 ns
+*/
+module LZ77_Encoder(clk,reset,chardata,valid, encode,finish,offset,match_len,char_nxt);
 input 				clk;
 input 				reset;
 input 		[7:0] 	chardata;
@@ -11,183 +16,175 @@ output 	 	[7:0] 	char_nxt;
 assign encode = 1;
 
 // Define Width
-parameter Wsearch = 9;				// Search buffer	=>  9 chars
-parameter Wchar   = 8;				// char  			=>  8 bits
-parameter In_len  = 2049;			// 2049 // 22 // 1025
-parameter rdn_len = Wsearch - 3;	// Redundant length for in_str
-parameter Wimg    = 12;				// img_length: 2049 => 12 bits
-parameter Wstate  = 2;				// state 0-3        =>  3 bits
+parameter [4:0] Wchar = 8;			// char  			 =>  8 bits
+parameter [4:0] Search_len = 9;		// Search buffer     =>  9 chars
+parameter [4:0] Look_len = 8;		// Look-ahead buffe  =>  8 chars
+parameter In_len = 2049 - Look_len;	// 2049 // 22
+parameter W_inlen = 12;				// img_length: 2041  => 12 bits
+parameter Wstate = 3;				// state 0-4         =>  3 bits
 
 // Constant
-parameter   [Wchar-1:0]	    EndSgn = 8'h24; // Dollar sign: '$' 
+parameter   [Wchar-1:0]	    EndSgn = 8'h24; // Dollar sign: '$'
 
 // Define State
-parameter 	[Wstate-1:0] 	In_S   = 0;
-parameter 	[Wstate-1:0] 	Enc_S  = 1;
-parameter 	[Wstate-1:0] 	Out_S  = 2;
-parameter 	[Wstate-1:0] 	Fin_S  = 3;
+parameter 	[Wstate-1:0] 	In_S0   = 0;
+parameter 	[Wstate-1:0] 	In_S1   = 1;
+parameter 	[Wstate-1:0] 	Enc_S   = 2;
+parameter 	[Wstate-1:0] 	Out_S   = 3;
+parameter 	[Wstate-1:0] 	Shift_S = 4;
+parameter 	[Wstate-1:0] 	Res_S   = 5;
+parameter 	[Wstate-1:0] 	Fin_S   = 6;
 
 
 // Output register
 reg 			valid;
 reg 			finish;
-reg [3:0] 		offset,    ans_offset, c_ml;
-reg [2:0] 		match_len, ans_match_len;
+reg [3:0] 		offset,    ans_offset;
+reg [2:0] 		match_len, ans_match_len, c_ml, shift_cnt;
 reg [Wchar-1:0] char_nxt;
 
 /********** Variables **********/
-reg [Wstate-1:0] 	cur_S, nxt_S;
-reg [Wchar-1:0]		in_str [0:In_len + rdn_len - 1]; // [In_len-1:0]
-reg [Wimg-1:0] 		char_cnt, ans_char_cnt, sb, lb; // Index: 0 - 2048
+reg [Wstate-1:0] 	cur_S, nxt_S, ctrl_sig;
+reg [Wchar-1:0]		sl_buf [0:Search_len+Look_len-1]; // search & look-ahead buffer
+reg [Wchar-1:0]		in_str [0:In_len-1]; // search & look-ahead buffer
+reg [W_inlen-1:0] 	i;                   // Index: 0 - 2040
+reg [4:0]           sl_ind;              // Index: 0 - 16
 
 // Next State Logic
 always @(*) begin
-	case (cur_S)
-		In_S: 	// STATE 0
-			nxt_S = (char_cnt == In_len)? Out_S : In_S;
-		Enc_S: 	// STATE 2
-			nxt_S = (sb + char_cnt+1) < lb ? Enc_S : Out_S;
-		Out_S: 	// STATE 3
-			nxt_S = in_str[lb + ans_match_len] == EndSgn ? Fin_S : Enc_S;
-		Fin_S: 	// STATE 4
-			nxt_S = Fin_S;
-		default: nxt_S = In_S;
-	endcase
+    case (cur_S)
+        In_S0:   nxt_S = (sl_ind == (Search_len + Look_len) - 1)?        In_S1 : In_S0;
+        In_S1:   nxt_S = (i == In_len)?                                  Out_S : In_S1;
+        Enc_S:   nxt_S = (sl_ind == Search_len - 1)?                     Out_S : Enc_S;
+        Out_S:   nxt_S = (sl_buf[Search_len + ans_match_len] == EndSgn)? Fin_S : Shift_S;
+        Shift_S: nxt_S = (shift_cnt == ans_match_len)?                   Res_S : Shift_S;
+        Res_S:   nxt_S = Enc_S;
+        Fin_S:   nxt_S = Fin_S;
+        default: nxt_S = In_S0;
+    endcase
 end
 
 // State Register
 always @(posedge clk) begin
-	cur_S <= reset ? In_S : nxt_S;
+    cur_S <= reset ? In_S0 : nxt_S;
 end
 
 // Output Logic
 always @(*) begin
-	case (cur_S)
-		In_S: begin // STATE 0
-			valid     = 0;
-			offset    = 0;
-			match_len = 0;
-			char_nxt  = 0;
-			finish    = 0;
-		end
-		Enc_S: begin // STATE 1
-			valid     = 0;
-			offset    = 0;
-			match_len = 0;
-			char_nxt  = 0;
-			finish    = 0;
-		end
-		Out_S: begin // STATE 2
-			valid     = 1;
-			offset    = ans_offset;
-			match_len = ans_match_len;
-			char_nxt  = in_str[lb + ans_match_len];
-			finish    = 0;
-		end
-		Fin_S: begin // STATE 3
-			valid     = 0;
-			offset    = 0;
-			match_len = 0;
-			char_nxt  = 0;
-			finish    = 1;
-		end
-		default: begin
-			valid     = 0;
-			offset    = 0;
-			match_len = 0;
-			char_nxt  = 0;
-			finish    = 0;
-		end
-	endcase
+    ctrl_sig  = cur_S;
+    case (cur_S)
+        Out_S: begin
+            valid     = 1;
+            offset    = ans_offset;
+            match_len = ans_match_len;
+            char_nxt  = sl_buf[Search_len + ans_match_len];
+            finish    = 0;
+        end
+        Fin_S: begin
+            valid     = 0;
+            offset    = 0;
+            match_len = 0;
+            char_nxt  = 0;
+            finish    = 1;
+        end
+        default: begin
+            valid     = 0;
+            offset    = 0;
+            match_len = 0;
+            char_nxt  = 0;
+            finish    = 0;
+        end
+    endcase
 end
-
-//  Test Matching Function
-parameter sb_test = 10;
-parameter lb_test = 13;
 
 // String Matching (Comb. ckt.)
+// match_len: 0-7
 wire [55:0] bundle_s, bundle_l, bundle_xor;
-assign bundle_s = {	in_str[sb + char_cnt],
-					in_str[sb + char_cnt + 1],
-					in_str[sb + char_cnt + 2],
-					in_str[sb + char_cnt + 3],
-					in_str[sb + char_cnt + 4],
-					in_str[sb + char_cnt + 5],
-					in_str[sb + char_cnt + 6]};
-assign bundle_l = {	in_str[lb],
-					in_str[lb+1],
-					in_str[lb+2],
-					in_str[lb+3],
-					in_str[lb+4],
-					in_str[lb+5],
-					in_str[lb+6]};
+assign bundle_s = {	sl_buf[sl_ind],
+                    sl_buf[sl_ind + 1],
+                    sl_buf[sl_ind + 2],
+                    sl_buf[sl_ind + 3],
+                    sl_buf[sl_ind + 4],
+                    sl_buf[sl_ind + 5],
+                    sl_buf[sl_ind + 6]};
+assign bundle_l = {	sl_buf[9],
+                    sl_buf[10],
+                    sl_buf[11],
+                    sl_buf[12],
+                    sl_buf[13],
+                    sl_buf[14],
+                    sl_buf[15]};
 assign bundle_xor = bundle_s ^ bundle_l;
 always @(*) begin
-	if(reset)
-		c_ml = 3'd0;
-	else begin
-		casex(bundle_xor)
-		56'h00000000000000: c_ml = 7;
-		56'h000000000000xx: c_ml = 6;
-		56'h0000000000xxxx: c_ml = 5;
-		56'h00000000xxxxxx: c_ml = 4;
-		56'h000000xxxxxxxx: c_ml = 3;
-		56'h0000xxxxxxxxxx: c_ml = 2;
-		56'h00xxxxxxxxxxxx: c_ml = 1;
-		default: 			c_ml = 0;
-		endcase
-	end
+    if (reset)
+        c_ml = 3'd0;
+    else begin
+        casex(bundle_xor)
+            56'h00000000000000: c_ml = 7;
+            56'h000000000000xx: c_ml = 6;
+            56'h0000000000xxxx: c_ml = 5;
+            56'h00000000xxxxxx: c_ml = 4;
+            56'h000000xxxxxxxx: c_ml = 3;
+            56'h0000xxxxxxxxxx: c_ml = 2;
+            56'h00xxxxxxxxxxxx: c_ml = 1;
+            default: 			c_ml = 0;
+        endcase
+    end
 end
 
-// Counter & Encoding
-integer i;
+// Datapath: Encoding
 // Quartus: Only clk signal can be triggered
-always @(posedge clk/* or cur_S*/) begin
-	for (i = In_len; i < In_len + rdn_len; i=i+1)
-		in_str[i] <= 0; // Initialize redundant reg
-	case(cur_S)
-		In_S: begin // STATE 0
-			sb 		<= 0;
-			lb 		<= 0;
-			ans_offset <= 0;
-			ans_match_len <= 0;
-			if(reset)
-				char_cnt <= 0;
-			else begin
-				if(char_cnt == In_len)
-					char_cnt <= 0;
-				else
-					in_str[char_cnt] <= chardata;
-					char_cnt <= char_cnt + 12'd1;
-			end
-		end
-		Enc_S: begin // STATE 2
-			sb <= sb; // sb_test;
-			lb <= lb; // lb_test;
-			char_cnt <= 
-				char_cnt + (sb + char_cnt + 12'd1 < lb ? 12'd1:12'd0);
-			if(c_ml > ans_match_len) begin
-				ans_offset <= lb - sb - char_cnt - 12'd1;
-				ans_match_len <= c_ml;
-			end
-		end
-		Out_S: begin // STATE 3
-			if(lb + ans_match_len - sb < Wsearch)
-				sb <= 0;
-			else
-				sb <= (lb + ans_match_len + 12'd1) - Wsearch;
-			lb <= lb + ans_match_len + 12'd1;
-			char_cnt <= 0;
-			ans_offset <= 0;
-			ans_match_len <= 0;
-		end
-		default: begin
-			sb <= 0;
-			lb <= 0;
-			char_cnt <= 0;
-			ans_offset <= 0;
-			ans_match_len <= 0;
-		end
-	endcase
+integer j;
+always @(posedge clk) begin
+    case(ctrl_sig)
+        In_S0: begin
+            ans_offset     <= 0;
+            ans_match_len  <= 0;
+            sl_buf[sl_ind] <= chardata;
+            i <= 0;
+            if (reset)
+                sl_ind <= Search_len;
+            else
+                sl_ind <= sl_ind + 5'd1;
+            for(j = 0; j < Search_len; j = j + 1)
+                sl_buf[j] = EndSgn;
+            shift_cnt <= 0;
+        end
+        In_S1: begin
+            ans_offset    <= 0;
+            ans_match_len <= 0;
+            sl_ind        <= 0;
+            in_str[i]     <= chardata;
+            i <= i + 12'd1;
+            shift_cnt <= 0;
+        end
+        Enc_S: begin
+            if(c_ml > ans_match_len) begin
+                ans_offset    <= 4'd8 - sl_ind[3:0];   // Search_len - 1 - sl_ind
+                ans_match_len <= c_ml;
+            end
+            sl_ind    <= sl_ind + 5'd1;
+            shift_cnt <= 0;
+        end
+        Out_S: begin
+            sl_ind    <= 0;
+            shift_cnt <= 0;
+        end
+        Shift_S: begin
+            for(j = 0; j < (Search_len + Look_len) - 1; j = j + 1)
+                sl_buf[j] <= sl_buf[j + 1];
+            sl_buf[(Search_len + Look_len) - 1] <= in_str[0];
+            for(j = 0; j < In_len - 1; j = j + 1)
+                in_str[j] <= in_str[j + 1];
+            shift_cnt <= shift_cnt + 3'd1;
+        end
+        Res_S: begin
+            sl_ind        <= 0;
+            ans_offset    <= 0;
+            ans_match_len <= 0;
+        end
+        default: begin
+        end
+    endcase
 end
 endmodule
